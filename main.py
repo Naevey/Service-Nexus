bc6625a3557d2a2042e151d3183e9bf1e260dc45
Dylan <dragonfly.luo@gmail.com>
2022-06-18 15:37:02 -0700

    Created the Home page

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
-from flask import render_template, request
-import requests
-import json
-from starter import app_starter
-from algorithm import app_algorithm
-from webapi import app_api
-from dc_crud.app_crud import app_crud
-from y2022 import app_y2022
-from web.websiteSearch import websiteSearch
-from __init__ import app
+from flask import render_template, request, Flask
+from pathlib import Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
+import json, requests
 
-app.register_blueprint(app_starter)
-app.register_blueprint(app_algorithm)
-app.register_blueprint(app_api)
-app.register_blueprint(app_crud)
-app.register_blueprint(app_y2022)
-app.register_blueprint(websiteSearch)
+# create a Flask instance
+app = Flask(__name__)
 
 @app.route('/')
-def index:
-    return render_template("index.html")
+def Home():
+    return render_template("Home.html")
 
 
 @app.route('/AboutUs/')
-def AboutUs
+def AboutUs():
     return render_template("AboutUs.html")
 
-# runs the application on the development server
 if __name__ == "__main__":
-    app.run(debug=True, port=8002)
+    app.run(debug=True, port=777)
